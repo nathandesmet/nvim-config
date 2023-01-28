c2fc89c6ba9d5d459517afee2cde9f0b60d0bbfc
Nathan Desmet <nathandesmet@users.noreply.github.com>
2023-01-28 18:17:35 +0100

    some useful keymaps

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -15,5 +15,14 @@ vim.keymap.set("n", "<c-l>", "4gt")
 
 vim.keymap.set("n", "<leader>tn", ":term<esc>")
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- [R]eplace [W]ord
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- [B]uffer [D]iff
+vim.keymap.set("n", "<leader>bd", [[:w !diff % -<cr>]])
+
 vim.cmd("command! NvimConfig :e /Users/nathandesmet/.config/nvim/")
 vim.cmd("command! W :w")
